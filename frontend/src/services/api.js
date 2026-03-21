e91e7859b1d2184c906f8cd1bfa69bd3dc663ac4
sanskaramle <sanskaramle.work@gmail.com>
2026-03-22 04:16:32 +0530

    fix api url for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AUTH_STORAGE_KEY, readStoredJson } from "../utils/storage";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
